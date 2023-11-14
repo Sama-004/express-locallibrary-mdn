ef93701b8a260c4fe092ccb15c5f37a288326d3d
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-11-14 18:24:21 +0530

    added catalog route

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const path = require("path");
 const cookieParser = require("cookie-parser");
 const logger = require("morgan");
 
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog"); //Import routes for "catalog" of the site
 
 const app = express();
 const mongoose = require("mongoose");
@@ -30,6 +31,7 @@ app.use(express.static(path.join(__dirname, "public")));
 //route handling
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
