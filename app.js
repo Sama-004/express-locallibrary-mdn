6af9b52345000b97f7ba43ce3e68aa3e695c1ae7
sama004 <70210929+Sama-004@users.noreply.github.com>
2023-11-21 23:53:48 +0530

    added express rate limit

--- app.js
+++ app.js
@@ -34,13 +34,15 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
 app.use("/catalog", catalogRouter);
-app.use(
-  helmet.contentSecurityPolicy({
-    directives: {
-      "script-src": ["'self'", "code.jquery.com", "cdn.jsdelivr.net"],
-    },
-  })
-);
+
+// Set up rate limiter: maximum of twenty requests per minute
+const RateLimit = require("express-rate-limit");
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
+// Apply rate limiter to all requests
+app.use(limiter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
